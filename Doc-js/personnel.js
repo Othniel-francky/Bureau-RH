1ba69765e800deaff26b36a990ca4abd4229af01
othniel francky <kouamefranckolivier45@gmail.com>
2023-03-23 17:05:33 +0000

    forcé

--- Doc-js/personnel.js
+++ Doc-js/personnel.js
@@ -7,3 +7,143 @@ function popup(event) {
 function close(){
     document.getElementById('contactModal').style.display = 'none';
 }
+
+function validateForm() {
+    let num = document.getElementById("num").value;
+    let name = document.getElementById("name").value;
+    let Matricule = document.getElementById("Matricule").value;
+    let Genre= document.getElementById("Genre").value;
+    let Mobile = document.getElementById("Mobile").value;
+    let Email= document.getElementById("Email").value;
+    let Date = document.getElementById("Date").value;
+    let Adresse= document.getElementById("Adresse").value;
+    let Action = document.getElementById("Action").value;
+    
+
+    if (num == "") {
+        alert("num is required");
+        return false;
+    }
+    if (name == "") {
+        alert("name is required");
+        return false;
+    }
+    if (Matricule == "") {
+        alert("Matricule is required");
+        return false;
+    }
+    if (Genre == "") {
+        alert("Genre is required");
+        return false;
+    }
+    if (Mobile == "") {
+        alert("Mobile is required");
+        return false;
+    }
+    if (Email == "") {
+        alert("Email is required");
+        return false;
+    } 
+    else if(!Email.includes("@")){
+        alert("Email invalid");
+        return false;
+    }
+    if (Date == "") {
+        alert("Date is required");
+        return false;
+    }
+    if (Adresse == "") {
+        alert("Adresse is required");
+        return false;
+    }
+    if (Action == "") {
+        alert("Action is required");
+        return false;
+    }
+
+    return true;
+}
+
+function showData() {
+    var peopleList;
+    if (localStorage.getItem("peopleList")==null) {
+        peopleList = [];
+    }
+    else{
+        peopleList = JSON.parse(localStorage.getItem("peopleList"))
+    }
+
+    const html = "";
+    peopleList.forEach(function(element,index){
+        html += "<tr>";
+        html += "<td>" + element.num + "</td>"
+        html += "<td>" + element.name + "</td>"
+        html += "<td>" + element.Matricule + "</td>"
+        html += "<td>" + element.Genre + "</td>"
+        html += "<td>" + element.Mobile + "</td>"
+        html += "<td>" + element.Email + "</td>"
+        html += "<td>" + element.Date + "</td>"
+        html += "<td>" + element.Adresse + "</td>"
+        html += "<td>" + element.Action + "</td>"
+        html += 
+        '<td><button onclick="deleteData(' + index +
+         ')" class="btn btn-danger">Delete</button><button onclick="updateData(' + index + 
+         ')" class="btn btn-warning m-2">Edit</button></td>';
+        html +="</tr>";
+    });
+
+    document.querySelector("#ajtTable tbody").innerHTML = 
+    html;
+}
+//charge toutes les données lors du chargement de documents ou de pages
+
+document.onload = showData();
+
+//fonction pour ajouter des données
+function addData() {
+//si le formulaire est validé
+
+    if (validateForm() == true) {
+        let num = document.getElementById("num").value;
+        let name = document.getElementById("name").value;
+        let Matricule = document.getElementById("Matricule").value;
+        let Genre= document.getElementById("Genre").value;
+        let Mobile = document.getElementById("Mobile").value;
+        let Email= document.getElementById("Email").value;
+        let Date = document.getElementById("Date").value;
+        let Adresse= document.getElementById("Adresse").value;
+        let Action = document.getElementById("Action").value;
+
+        var peopleList;
+        if (localStorage.getItem("peopleList")==null) {
+            peopleList = [];
+        }
+        else{
+            peopleList = JSON.parse(localStorage.getItem("peopleList"))
+        }
+
+        peopleList.push({
+            num : num,
+            name : name,
+            Matricule : Matricule,
+            Genre : Genre,
+            Mobile : Mobile,
+            Email : Email,
+            Date : Date,
+            Adresse : Adresse,
+            Action : Action,
+        })
+
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById("num").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("Matricule").value = "";
+        document.getElementById("Genre").value = "";
+        document.getElementById("Mobile").value = "";
+        document.getElementById("Email").value = "";
+        document.getElementById("Date").value = "";
+        document.getElementById("Adresse").value = "";
+        document.getElementById("Action").value = "";
+    }
+}
--- Doc-js/taches.js
+++ Doc-js/taches.js
@@ -3,26 +3,141 @@
 document.getElementById('ajout').addEventListener('click', popup)
 function popup(event) {
     document.getElementById('contactModal').style.display = 'block';
-    document.querySelector('.close').addEventListener('click',close)
+    document.querySelector('.close').addEventListener('click',close);
 }
 
 function close(){
     document.getElementById('contactModal').style.display = 'none';
 }
 
-document.querySelector('.addContactButton').addEventListener('click',saveTache)
-function saveTache(){
-    const name = document.getElementById('name');
-    const tache = document.getElementById('tache');
-    const Priorités = document.getElementById('Priorités');
-    const taxeAmeliorationaches = document.getElementById("Axe d'Amelioration");
-    const TypesTaches = document.getElementById('Types de Taches');
-    const typePaiement = document.getElementById('Type de Paiement');
 
-    document.querySelector('tache');
-    document.querySelector('Priorités');
-    document.querySelector("Axe d'Amelioration");
-    document.querySelector('Types de Taches');
-    document.querySelector('Type de Paiement');
-    const  Pourcentage = document.getElementById('Pourcentage');
+document.getElementById('ajouter').addEventListener('click',save);
+function save() {
+
+     const Taches = document.getElementById('Taches');
+    const Priorites = document.getElementById('Priorites');
+    const Pourcentage = document.getElementById('Pourcentage');
+    const Axe = document.getElementById("Axe");
+    const categorie = document.getElementById('categorie');
+    const Paiement = document.getElementById('Paiement');
+
+    if (Taches.value !=  "" &&  Taches.value.replaceAll(" ", "")!="") {
+        if (Priorites.value !=  "" &&  Priorites.value.replaceAll(" ", "")!="") {
+            if (Pourcentage.value !=  "" &&  Pourcentage.value.replaceAll(" ", "")!="") {
+                if (Axe.value !=  "" &&  Axe.value.replaceAll(" ", "")!="") {
+                    if (categorie.value !=  "" &&  categorie.value.replaceAll(" ", "")!="") {
+                        if (Paiement.value !=  "" &&  Paiement.value.replaceAll(" ", "")!="") {
+                            const tab = [];
+                            const donneTaches = {
+                                id : "",
+                                nom : Taches.value,
+                                Priorites : Priorites.value,
+                                pourcentages : Pourcentage.value,
+                                Axe: Axe.value,
+                                categorie : categorie.value,
+                                paiement : Paiement.value
+                            }
+                            Taches.value =  "" ;
+                            Priorites.value =  "";
+                            Pourcentage.value =  "";
+                            Axe.value =  "";
+                            categorie.value =  "";
+                            Paiement.value =  "";
+
+                            document.getElementById("contactModal").style.display = "none";
+
+
+                            if (!localStorage.getItem('Taches')){
+                                donneTaches.id="TA-1";
+                                tab.push(donneTaches);
+                                localStorage.setItem("Taches",JSON.stringify(tab));
+                            }else{
+                                const ref = JSON.parse(localStorage.getItem("Taches"));
+                                donneTaches.id="TA-"+(ref.length+1);
+                                ref.push(donneTaches);
+                                localStorage.setItem("Taches",JSON.stringify(ref));
+                            }
+                        }else{
+                            Paiement.focus()
+                        }
+                    }else{
+                        categorie.focus()
+                    }
+                }else{
+                    Axe.focus()
+                }
+            }else{
+                Pourcentage.focus()
+            }
+        }else{
+            Priorites.focus()
+        }
+    }else{
+        Taches.focus()
+    }
+
 }
+const tcorps = document.getElementById("tcorps");
+const mesTaches = localStorage.getItem("Taches");
+if(mesTaches){
+    JSON.parse(mesTaches).forEach(element => {
+        const tr = document.createElement("tr");
+        tr.id = `ligne-${element.id}`;
+        tcorps.append(tr);
+
+        const identifiant = document.createElement("td");
+        identifiant.id = element.id;
+        identifiant.textContent = element.id;
+        tr.append(identifiant);
+
+        const tache = document.createElement("td");
+        tache.id = "nom-"+element.id;
+        tache.textContent = element.nom;
+        tr.append(tache);
+
+        const prior = document.createElement("td");
+        prior.id = "Priorites-"+element.id;
+        prior.textContent = element.Priorites;
+        tr.append(prior);
+
+        const pourcent = document.createElement("td");
+        pourcent.id = "pourcentites-"+element.id;
+        pourcent.textContent = element.pourcentages;
+        tr.append(pourcent);
+
+        const ax = document.createElement("td");
+        ax.id = "axe-"+element.id;
+        ax.textContent = element.Axe;
+        tr.append(ax);
+
+        const typo = document.createElement("td");
+        typo.id = "categorie-"+element.id;
+        typo.textContent = element.categorie;
+        tr.append(typo);
+        console.log(element.categorie)
+
+        const paye = document.createElement("td");
+        paye.id = "paiement-"+element.id;
+        paye.textContent = element.paiement;
+        tr.append(paye);
+
+        const colbotton = document.createElement("td");
+        colbotton.id = "colbotton-"+element.id;
+        tr.append(colbotton);
+
+        const mbotton = document.createElement("button");
+        mbotton.id = "modifier-"+element.id;
+        mbotton.textContent = "Modifier";
+        colbotton.append(mbotton);
+
+        const sbotton = document.createElement("button");
+        sbotton.id = "supprimer-"+element.id;
+        sbotton.textContent = "Supprimer";
+        colbotton.append(sbotton);
+
+
+    });
+    
+}
+
+
